34dfcc04082a2b15ba937973a3888d125beefa14
Jerry Ku <jeku@be-linux35.lt.ucsc.edu>
2016-09-28 17:26:24 -0700

    I think I'm done

---
+++ create.sql
@@ -0,0 +1,36 @@
+DROP SCHEMA lab1 CASCADE;
+CREATE SCHEMA lab1;
+ALTER ROLE jeku SET SEARCH_PATH TO lab1;
+CREATE TABLE stores
+(	storeID INT,
+	address VARCHAR(255),
+	Manager CHAR(30),
+	PRIMARY KEY(storeID)
+);
+
+CREATE TABLE customers
+(	customerID INT,
+	name TEXT,
+	address TEXT,
+	email TEXT,
+	PRIMARY KEY(customerID)
+);
+
+CREATE TABLE products
+(	productID INT,
+	name TEXT,
+	category TEXT,
+	manufacturer TEXT,
+	PRIMARY KEY(productID)
+);
+
+CREATE TABLE sales
+(	storeID INT,
+	customerID INT,
+	productID INT,
+	date TEXT,
+	quantity INT,
+	price INT,
+	shipped BOOLEAN,
+	PRIMARY KEY(storeID, customerID, productID, date)
+);
---
+++ drop.sql
@@ -0,0 +1,5 @@
+DROP TABLE stores;
+DROP TABLE customers;
+DROP TABLE products;
+DROP TABLE sales;
+
